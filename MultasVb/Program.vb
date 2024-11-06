c6199e078d64d24f0b0cd276edaa34f535dc9da8
LeanCG <leandrocorrea040@gmail.com>
2024-11-06 04:25:26 -0300

    ejercicio multas

--- MultasVb/Program.vb
+++ MultasVb/Program.vb
@@ -1,7 +1,34 @@
 Imports System
 
 Module Program
+    dim velocidadMaxima() as intger = {20,40,60,100}
+    Enum puntoControl
+        altoriesgo 
+        calle
+        avenida
+        autopista
+    End Enum
+
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
     End Sub
+
+    Sub Imprimir(cadena As String)
+        Console.WriteLine("{0}",cadena)
+    End Sub
+
+    function CondicionSalida()
+        Imprimir("Desea Procesar otra Multa? Si/No  escriba Si para continuar. No para Salir")
+        Condicion = Console.ReadLine()
+        ValidacionCondicionSalida(Condicion)
+        If Condicion = "No"
+            return break
+        End If
+    End function
+    function ValidacionCondicionSalida(Condicion as String)
+        While Condicion <> "Si" And Condicion <> "No"
+            Imprimir("Error: escriba Si para continuar. No para Salir ")
+            Condicion = Console.ReadLine() 
+        End While
+    End function
 End Module
